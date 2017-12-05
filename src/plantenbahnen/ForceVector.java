7904e757bd843730e7d42b4cff361cb9b4b5ef01
jonathan <jonathan@fhi-berlin.mpg.de>
2017-12-05 14:43:34 +0100

    typoo

--- src/plantenbahnen/ForceVector.java
+++ src/plantenbahnen/ForceVector.java
@@ -1,5 +1,5 @@
 public class SpaceObject{
-    double[] directionVector 0  new double[2];
+    double[] directionVector =  new double[2];
     double force;
 
     SpaceObject(double[] directionVector,double force){
